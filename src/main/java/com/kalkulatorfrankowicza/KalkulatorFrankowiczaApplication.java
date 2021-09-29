418c753a3c2844d918442a2adebf9687260297da
tkorulczyk <tomasz.korulczyk@gmail.com>
2021-09-29 12:02:45 +0200

    Initial commit

---
+++ src/main/java/com/kalkulatorfrankowicza/KalkulatorFrankowiczaApplication.java
@@ -0,0 +1,13 @@
+package com.kalkulatorfrankowicza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KalkulatorFrankowiczaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KalkulatorFrankowiczaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kalkulatorfrankowicza/KalkulatorFrankowiczaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kalkulatorfrankowicza;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KalkulatorFrankowiczaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
